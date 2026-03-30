5fa58e6fa6cff1821d83f9487ef0d4046b0cfe71
tushar090803 <tushargupta140507@gmail.com>
2026-03-30 19:00:29 +0530

    30march26

---
+++ checkIfStringsCanbeMadeEqualWithOperationsII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        string s11,s12,s21,s22;
+        for(int i=0;i<s1.size();i++){
+           if(i&1){
+            s12+=s1[i];
+            s22+=s2[i];
+           } 
+           else{
+            s11+=s1[i];
+            s21+=s2[i];
+           }
+        }
+        sort(s11.begin(),s11.end());
+        sort(s12.begin(),s12.end());
+        sort(s21.begin(),s21.end());
+        sort(s22.begin(),s22.end());
+        return s11==s21&&s12==s22;
+    }
+};
